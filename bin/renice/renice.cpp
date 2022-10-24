926d9e5b4697faf85f8abf3b42c88be4a17d7744
Alexander Du <87888336+du-alexander@users.noreply.github.com>
2022-10-23 22:13:01 -0700

    renice.cpp sconscript

--- bin/renice/renice.cpp
+++ bin/renice/renice.cpp
@@ -3,15 +3,19 @@
 #include <string.h>
 #include <ProcessClient.h>
 #include <unistd.h>
-#include <types.h>
 #include <Process.h>
 #include "renice.h"
 
+int prio;
+ProcessID pid;
+
 renice::renice(int argc, char **argv) : POSIXApplication(argc, argv){
     parser().setDescription("Change the priority of a process");
+    parser().registerFlag('n', "setPriority", "Changes process priority to new priority");
     parser().registerPositional("PRI", "Priority to change the process to");
     parser().registerPositional("PID", "PID of the process to change");
-    parser().registerFlag('n', "setPriority", "Changes process priority to new priority");
+    prio = atoi(argv[2]);
+    pid = atoi(argv[3]);
 }
 
 renice::~renice(){
@@ -19,11 +23,13 @@ renice::~renice(){
 }
 
 renice::Result renice::exec(){
-    //ProcessID pid = atoi(arguments().get("PID"));
-    //int prio = atoi(arguments().get("PRI"));
-    int prio = atoi(argv[2]);
-    ProcessID pid = atoi(argv[3]);
+    // ProcessID pid = atoi(arguments().get("PID"));
+    // int prio = atoi(arguments().get("PRI"));
 
+    // int prio = atoi(argv[2]);
+    // ProcessID pid = atoi(argv[3]);
+
+    ProcessClient process;
     if (pid <=  0 || pid >= ProcessClient::MaximumProcesses)
     {
         ERROR("Invalid PID: " << pid);
@@ -32,8 +38,8 @@ renice::Result renice::exec(){
         ERROR("Invalid priority: " << prio);
         return InvalidArgument;
     } else {
-        process.setPriority(prio);
-        out << "Priority Changed!!!!!!!!!!\r\n";
+        process.setPriority(pid, prio);
+        //out << "Priority Changed!!!!!!!!!!\r\n";
     }
 
     return Success;
